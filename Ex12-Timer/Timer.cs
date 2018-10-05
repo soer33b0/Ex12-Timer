3c0bda6d0dfda476ae9a66539db8fa2fd631b1a9
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2018-10-05 11:08:36 +0200

    Add project files.

---
+++ Ex12-Timer/Timer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12_Timer
+{
+    public class Timer
+    {
+        public int Seconds { get; set; }
+
+        public override string ToString()
+        {
+            int sec = (Seconds % 60);
+            int min = ((Seconds / 60) % 60);
+            int hours = (Seconds / 3600);
+
+            return Padded(hours) + ":" + Padded(min) + ":" + Padded(sec);
+
+            /*if(hours.Length < 2)
+            {
+                hours = "0" + hours;
+            }
+            
+            if(min.Length < 2)
+            {
+                min = "0" + min;
+            }
+
+            if (sec.Length < 2)
+            {
+                sec = "0" + sec;
+            }
+
+            return hours + ":" + min + ":" + sec;*/
+        }
+
+        public string Padded(int v)
+        {
+            string result;
+
+            if (v < 10)
+            {
+                result = "0" + v;
+            }
+
+            else
+            {
+                return v.ToString();
+            }
+
+            return result;
+        }
+    }
+}
